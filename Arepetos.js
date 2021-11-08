11d2053a69059074a6c38956ac372b00ade6862d
miguel <jmtiradop19@gmail.com>
2021-11-08 11:22:30 -0500

    nuevas imagenes y responsive

--- Arepetos.js
+++ Arepetos.js
@@ -1,3 +1,10 @@
+function cambiarClase(){
+    let siteNav = document.getElementById('site-nav')
+    siteNav.classList.toggle('site-nav-open')
+    let menuOpen = document.getElementById('menu-toggle');
+    menuOpen.classList.toggle('menu-open')
+};
+
 var slideIndex = 0;
 showSlides();
 
