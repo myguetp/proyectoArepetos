407c52b1dd6557b70ffc2510ee4317b376f7a2f2
miguel <jmtiradop19@gmail.com>
2021-11-15 18:21:32 -0500

    avance de conocenos

---
+++ conocenos.js
@@ -0,0 +1,20 @@
+function mostrar(){
+    document.getElementById('contenido').style.display="block";
+}
+function ocultar(){
+    document.getElementById('contenido').style.display="none";
+};
+
+function mostrar2(){
+    document.getElementById('contenido2').style.display="block";
+}
+function ocultar2(){
+    document.getElementById('contenido2').style.display="none";
+};
+
+function mostrar3(){
+    document.getElementById('contenido3').style.display="block";
+}
+function ocultar3(){
+    document.getElementById('contenido3').style.display="none";
+};
